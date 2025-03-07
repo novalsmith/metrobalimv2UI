64cbe33e34fd220fd9174a7cc991c665819eb426
Noval Smith <novalsmith69@gmail.com>
2025-03-07 07:11:17 +0700

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,73 +1,44 @@
 <template>
     <v-app>
-        <!-- App Bar -->
         <v-app-bar fixed app height="150">
             <v-container>
-                <v-row align="center" justify="space-between">
-                    <!-- Logo -->
-                    <v-col cols="3" class="d-flex justify-start align-center">
-
-
-                        <Ads :src="imgLogo" alt="Iklan Metro Bali" :modifiers="{
-                            format: 'webp',
-                            quality: 80,
-                            width: 200
-                        }" @click="handleImageAdClick" />
-                    </v-col>
-
-                    <!-- Search Bar -->
-                    <v-col cols="6" class="d-flex justify-center align-center">
-                        <v-text-field class="mt-3" density="compact" variant="solo-filled" rounded
-                            prepend-inner-icon="mdi-magnify" label="Search">
-                        </v-text-field>
-                    </v-col>
-
-                    <!-- Right Buttons -->
-                    <v-col cols="3" class="d-flex justify-end align-center">
-                        <div class="mt-3">
-                            <v-btn>
-                                <v-icon>mdi-mail</v-icon> Subscribe
+                <v-toolbar density="compact" flat>
+                    <v-row align="center" no-gutters>
+                        <v-col cols="auto" class="d-flex align-center">
+                            <v-btn small icon @click="toggleMenu" class="mr-2">
+                                <v-icon>{{ menuIcon }}</v-icon>
                             </v-btn>
-                            <v-btn icon>
-                                <v-icon icon="mdi-bookmark-outline"></v-icon>
-                            </v-btn>
-                            <v-btn icon @click="toggleTheme" class="mr-2">
-                                <v-icon :icon="currentThemeIcon"></v-icon>
-                            </v-btn>
-                        </div>
-                    </v-col>
-
-                    <v-col cols="12">
-                        <template v-if="isLoading">
-                            <v-skeleton-loader type="text" width="100%" height="40" />
-                        </template>
-                        <template v-else>
-                            <v-btn v-for="(menu, i) in menus" :key="i" variant="text" class="text-capitalize"
-                                :to="menu.categoryId">
-                                {{ menu.title }}
-                            </v-btn>
-                            <!-- <v-spacer></v-spacer> -->
-                            <v-btn variant="text" @click="toggleMenu">
-                                Lainnya <v-icon>{{ menuIcon }}</v-icon>
-                            </v-btn>
-                        </template>
-                    </v-col>
-                </v-row>
+                            <Ads :src="imgLogo" alt="Iklan Metro Bali"
+                                :modifiers="{ format: 'webp', quality: 80, width: 200 }" @click="handleImageAdClick"
+                                class="mr-4" />
+                        </v-col>
+                        <v-col class="d-flex align-center">
+                            <v-text-field density="compact" variant="solo-filled" rounded
+                                prepend-inner-icon="mdi-magnify" label="Cari tokoh, topik, atau peristiwa"
+                                style="width: 500px; color: white;" class="pt-5 mr-2"
+                                @input="handleSearch"></v-text-field>
+                            <v-btn class="mr-2" @click="handleSubscribe">Langganan</v-btn>
+                            <v-btn icon small class="mr-2" @click="toggleTheme"><v-icon
+                                    :icon="currentThemeIcon"></v-icon></v-btn>
+                            <v-btn icon small @click="handleProfile"><v-icon>mdi-account-circle-outline</v-icon></v-btn>
+                        </v-col>
+                    </v-row>
+                </v-toolbar>
+                <v-toolbar density="compact" flat class="mt-5 border-top">
+                    <v-toolbar-items>
+                        <v-row no-gutters class="mt-3">
+                            <v-col cols="auto" v-for="(menu, i) in menus" :key="i" class="mr-2">
+                                <v-list-item density="comfortable" :to="menu.categoryId" variant="text">
+                                    <v-list-item-title class="text-capitalize">{{ menu.title }}</v-list-item-title>
+                                </v-list-item>
+                            </v-col>
+                        </v-row>
+                    </v-toolbar-items>
+                    <v-spacer></v-spacer>
+                </v-toolbar>
             </v-container>
         </v-app-bar>
 
-        <!-- Side Menu -->
-        <v-navigation-drawer v-model="sideMenu" temporary location="top" color="blue">
-            <v-container>
-                <v-list>
-                    <v-list-item v-for="(menu, i) in menus" :key="i" @click="navigate(menu)">
-                        <v-list-item-title>{{ menu.title }}</v-list-item-title>
-                    </v-list-item>
-                </v-list>
-            </v-container>
-        </v-navigation-drawer>
-
-        <!-- Main Content -->
         <v-main>
             <keep-alive>
                 <NuxtPage />
@@ -75,14 +46,22 @@
         </v-main>
         <ArticleDetailDialog ref="articleDialog" />
 
-        <!-- Footer -->
-        <v-footer>
-            <v-col class="text-center py-3">
-                <span>&copy; 2025 Metrobalim. All Rights Reserved.</span>
-            </v-col>
-        </v-footer>
+        <v-footer><v-col class="text-center py-3"><span>&copy; 2025 Metrobalim. All Rights
+                    Reserved.</span></v-col></v-footer>
+
     </v-app>
+    <v-navigation-drawer v-model="sideMenu" temporary location="left" color="blue">
+        <v-container>
+            <v-list>
+                <v-list-item v-for="(menu, i) in menus" :key="i" @click="navigate(menu)">
+                    <v-list-item-title>{{ menu.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-container>
+    </v-navigation-drawer>
 </template>
+
+
 
 <script setup>
 import { ref, computed, onMounted } from 'vue';
@@ -142,27 +121,43 @@ const isActive = (path) => {
 };
 </script>
 
-<style scoped>
-.v-app-bar {
-    display: block !important;
-    visibility: visible !important;
-}
 
+<style scoped>
 .v-navigation-drawer {
     width: 250px;
-}
-
-.v-btn {
-    font-size: 14px;
-    padding: 10px;
 }
 
 .text-right {
     text-align: right;
 }
 
-.article-image img {
-    object-fit: contain;
-    width: 100%;
+.border-top {
+    border-top: 1px solid rgb(195, 195, 195);
+}
+
+.toolbar-menu-item {
+    padding: 0 8px;
+    /* Sesuaikan padding horizontal */
+    margin: 0;
+    /* Hilangkan margin */
+    font-size: 13px;
+    /* Sesuaikan ukuran font */
+}
+
+.toolbar-menu-item.v-btn--active {
+    background-color: white !important;
+    /* Hilangkan latar belakang aktif */
+}
+
+.v-list-item {
+    padding: 0 8px;
+    /* Sesuaikan padding horizontal */
+    font-size: 13px;
+    /* Sesuaikan ukuran font */
+}
+
+/* Hilangkan background abu-abu pada v-toolbar */
+.v-toolbar {
+    background-color: transparent !important;
 }
 </style>
